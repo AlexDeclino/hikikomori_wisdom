2aea372ac1ad7e9dde08b9f27396cbeaa8f385cc
gvn908 <gvnergy@gmail.com>
2024-01-02 15:58:03 +0100

    added OR prophecy

--- scripts/prophecy.gd
+++ scripts/prophecy.gd
@@ -13,22 +13,40 @@ var prophecy_finished = preload("res://assets/sfx/1054.wav")
 var hand_cursor = preload("res://assets/cursors/hand_knock.png")
 var arrow_cursor = preload("res://assets/cursors/hand_normal.png")
 
-#reaction texts
-var reaction_text = [
-	"she might not be awake",
-	"reaction time enganged",
-	"swat time active"
-]
+var random_pics = [
+		preload("res://_tests/1.png"),
+		preload("res://_tests/2.png"),
+		preload("res://_tests/3.png")
+	]
+
+#lists
+var reactions_array = []
+var words_array = []
+
 
 func _ready():
 	Input.set_custom_mouse_cursor(arrow_cursor, Input.CURSOR_ARROW, Vector2(20,20))
 	Input.set_custom_mouse_cursor(hand_cursor, Input.CURSOR_POINTING_HAND, Vector2(24,24))
 	$AnimationPlayer.play("intro_fade-in")
 	
+	#load all words lists into arrays
+	_load_file_list("res://assets/texts/reactions.txt", reactions_array)
+	_load_file_list("res://assets/texts/words.txt", words_array)
+	
+
+	#apply settings for cursor and focus to all buttons
 	var buttons = get_tree().get_nodes_in_group("buttons")
 	for i in buttons:
 		i.focus_mode = FOCUS_NONE
 		i.mouse_default_cursor_shape = CURSOR_POINTING_HAND
+
+func _load_file_list(file2open, array2append):
+	var _file = FileAccess.open(file2open, FileAccess.READ)
+	while not _file.eof_reached():
+		var line = _file.get_line()
+		array2append.append(line)
+	_file.close()
+	array2append.pop_back()
 
 func _play_sfx(sfx):
 	$sounds.stream = sfx
@@ -66,9 +84,9 @@ func _on_knock_timer_timeout():
 			_play_sfx(prophecy_calculating)
 
 func _print_reaction():
-	reaction_text.shuffle()
-	get_node(nodes_paths[5]).text = reaction_text.front()
-	reaction_text.pop_front()
+	reactions_array.shuffle()
+	get_node(nodes_paths[5]).text = reactions_array.front()
+	reactions_array.pop_front()
 
 #---------- stage 0
 
@@ -76,12 +94,15 @@ func _print_reaction():
 #stage 1 ----------
 
 func _on_prophecy_timer_timeout():
+	random_pics.shuffle()
 	var prophecy_bar = get_node(nodes_paths[0])
+	var pic_selected = random_pics.front()
 	if prophecy_bar.value >= 100:
 		_prophecy_complete()
 	else:
 		prophecy_bar.value += 1
-
+	$prophecy_stages/encounter/TextureRect.texture = pic_selected
+	
 func _prophecy_complete():
 	_play_sfx(prophecy_finished)
 	get_node(nodes_paths[1]).stop()
@@ -94,13 +115,13 @@ func _prophecy_complete():
 #stage 2 ----------
 
 func _select_prophecy():
-	var random = randi_range(2,2)
+	var random = randi_range(1,2)
 	var method = var_to_str(random)
 	match method:
 		"1":
-			_prophecy_word()
+			$prophecy_stages/prophecy/Label.text = _prophecy_word()
 		"2":
-			$prophecy_stages/prophecy/Label.text = _prophecy_repeat()
+			$prophecy_stages/prophecy/Label.text = _prophecy_or()
 		"3":
 			_prophecy_sentence()
 		_:
@@ -108,19 +129,36 @@ func _select_prophecy():
 
 func _prophecy_word():
 	print("word")
+	var chosen
+	words_array.shuffle()
+	chosen = words_array.front()
+	words_array.pop_front()
+	return chosen
 	
-func _prophecy_repeat():
-	print("repeat")
-	var words_array = []
-	var words_file = FileAccess.open("res://assets/texts/words.txt", FileAccess.READ)
-	while not words_file.eof_reached():
-		var line = words_file.get_line()
-		words_array.append(line)
-	words_file.close()
-	words_array.pop_back()
-	return words_array[0]
+func _prophecy_or():
+	print("or")
+	var max_loops = 5
+	var loops = randi_range(2, max_loops)
+	var or_sentence:String
+	var current_word:String
+	for i in loops :
+		words_array.shuffle()
+		current_word = words_array.front()
+		words_array.pop_front()
+		if i == 0 :
+			or_sentence = current_word + " or "
+		elif i == loops - 1 :
+			or_sentence = or_sentence + current_word
+		else: 
+			or_sentence = or_sentence + current_word + " or " 
 		
-		
+#	words_selected.append(words_array.front())	
+#	for y in words_selected :
+#		if y == words_selected.back():
+#			or_sentence = y + or_sentence
+#		else:
+#			or_sentence = y + " or " + or_sentence
+	return or_sentence
 	
 
 func _prophecy_sentence():
@@ -128,9 +166,12 @@ func _prophecy_sentence():
 
 #---------- stage 2
 
-#var words_lines = []
-#	var content = words_file.get_as_text()
-#	words_lines = content.split("\n")
-#	words_file.close()
-#	var random_line_index = randi() % words_lines.size()
-#	return words_lines[random_line_index]
+#debug
+#func _on_button_pressed():
+#	DisplayServer.window_set_size(Vector2i(512,512))
+
+
+func _on_button_pressed():
+	$prophecy_stages.current_tab = 2
+	_select_prophecy()
+	
